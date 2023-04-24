bf76eb1858c62f667606d82e350696a6965dafab
nitipon <nitipon.apaisri@gmail.com>
2023-04-24 20:26:52 +0200

    Changed fetch url

--- models/coinModel.js
+++ models/coinModel.js
@@ -1,19 +1,14 @@
 const dummy_data = require("../data/dummy_data.json");
 const getCoins = async () => {
-    // const res = await fetch("https://api.coinranking.com/v2/coins", {
-    //     method: "GET",
-    //     headers: {
-    //         "Content-Type": "application/json",
-    //         "x-access-token": "process.env.COINRANKING_API_KEY",
-    //     },
-    // });
-    // const data = await res.json();
-
-    // const data = await fetch(dummy_data)
-    //     .then((res) => res.json())
-    //     .catch((err) => console.log(err));
-
-    return dummy_data;
+    const res = await fetch("https://api.coincap.io/v2/assets?limit=10", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.COINCAP_API_KEY}`,
+        },
+    });
+    const data = await res.json();
+    return data;
 };
 
 module.exports = { getCoins };
